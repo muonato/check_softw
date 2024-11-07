d2eeb9b645ad2bec901956f18dc327a8b5614a24
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-11-07 12:28:43 +0200

    Update check_softw.sh

--- check_softw.sh
+++ check_softw.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 #
-# muonato/check_softw.sh @ GitHub (16-DEC-2023)
+# muonato/check_softw.sh @ GitHub (07-NOV-2024)
 #
-# Reports software package version using rpm query,
-# compatible with Nagios monitoring as host plugin
+# Reports software package version using rpm query or by executing an 
+# arbitrary shell command. Compatible with Nagios monitoring as plugin.
 #
 # Usage:
-#       bash check_softw.sh <package-name> [<package-name>] ...
+#       bash check_softw.sh <package-name|command> [<package-name|command>] ...
 #
 #       Nagios nrpe configuration on host :
-#       command[check_rpmq]=/path/to/plugins/check_rpmq.sh $ARG1$
+#       command[check_softw]=/path/to/plugins/check_softw.sh $ARG1$
 #
 # Parameters:
-#       1..n: package name
+#       1..n: package name or shell command
 #
 # Examples:
-#       $ bash check_rpmq.sh fubar-one
-#       (Check single software package)
+#       Check single sw package using rpm query
+#       $ bash check_softw.sh "fubar-one"
 #
-#       check_nrpe -H $HOSTADDRESS$ -c check_rpmq -a 'fubar-one fubar-two'
-#       (Nagios monitor expression for two packages)
+#       Check using rpm query and shell commandline
+#       $ bash check_softw.sh "fubar-one" "psql --version"
+#
+#       Nagios plugin expression for two sw packages
+#       check_nrpe -H $HOSTADDRESS$ -c check_softw -a '"fubar-one" "fubar-two"'
+#
+#       Nagios plugin expression for apache in docker container 'foobar'
+#       check_nrpe -H $HOSTADDRESS$ -c check_softw -a '"docker exec foobar apache2 -v"'
 #
 # Platform:
 #       Red Hat Enterprise Linux 8.9 (Ootpa)
 #       Opsview Core 3.20140409.0
 #
-
 # BEGIN __main__
-umask 0077
-
 if [[ -z "$1" ]]; then
     echo -e "check software package version\n\tUsage:\
     `basename $0` <package-name> [<package-name>] ...\n
@@ -44,7 +47,7 @@ for (( i=1; i<=$#; i++ )); do
         SUM=$(echo $ARG|wc -w)
 
         if [[ $SUM -gt 1 ]]; then
-                VER=$(eval $ARG)
+                VER=$($ARG 2>&1)
         else
                 VER="$ARG $(rpm -qi $ARG|grep -i version)"
         fi
